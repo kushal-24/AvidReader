82b3be01942539e8b0f7f21b113bed58f868303d
kushal-24 <kushalphadnis2015@gmail.com>
2025-07-04 11:36:29 +0530

    store configured (RTK)

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,40 @@
-import { useState } from 'react'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
+import { useDispatch } from "react-redux";
+import authService from "./appWrite/auth.js";
+import { login, logout } from "./features/authSlice.js";
+import Header from "./components/header/Header.jsx";
+import Footer from "./components/header/footer/Footer.jsx";
+import { Outlet } from "react-router-dom";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
 
-  console.log(import.meta.env.VITE_APPWRITE_URL)
-  return (
-    <>
-      <h1>An app with appwrite!!</h1>
-    </>
-  )
-  
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  return !loading ? (
+    <div className="min-h-screen min-w-full text-black flex content-between bg-emerald-100">
+      <div className="w-full block">
+        <Header />
+        <main>
+          TODO <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
-export default App
+export default App;
---
+++ src/components/header/index.js
@@ -0,0 +1,7 @@
+import Header from "./Header";
+import Footer from "./footer/Footer";
+
+export { 
+    Header, 
+    Footer 
+};
